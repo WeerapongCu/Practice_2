93ee9911f55aa6c8cf9c3e7767b24f80aace75fc
= <=>
2024-02-27 01:08:49 +0700

    change name

--- note/note.go
+++ note/note.go
@@ -10,17 +10,17 @@ import (
 )
 
 type Note struct {
-	title     string
-	content   string
-	createdAt time.Time
+	Title     string `json:"abc"`
+	Content   string `json:"content`
+	CreatedAt time.Time `json:"created_at"`
 }
 
 func (note Note) Display() {
-	fmt.Printf("Your note titled %v has the following content:\n\n%v\n\n", note.title, note.content)
+	fmt.Printf("Your note titled %v has the following content:\n\n%v\n\n", note.Title, note.Content)
 }
 
 func (note Note) Save() error {
-	fileName := strings.ReplaceAll(note.title, " ", "_")
+	fileName := strings.ReplaceAll(note.Title, " ", "_")
 	fileName = strings.ToLower(fileName)
 
 	json, err := json.Marshal(note)
@@ -38,8 +38,8 @@ func New(title, content string)(Note, error) {
 	}
 
 	return Note{
-		title:     title,
-		content:   content,
-		createdAt: time.Now(),
+		Title:     title,
+		Content:   content,
+		CreatedAt: time.Now(),
 	}, nil
 }
